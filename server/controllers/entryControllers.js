447bdc4b680a65b6dd293fd5b5084deaa76dde70
Emmanuel Nkurunziza <ewink.01@gmail.com>
2019-10-26 05:11:11 +0200

    ft(ch2-modify-entry): Modify Entry API
    with this feature, the user should be able to:
    
    1.use the provided token,
    2.have an authorization token allowing user to modify a diary entry title and description
     with this feature we put to place to the following functionalities to build the modify entry API:
    1.make the editEntry method of the Controller4entry class
    2.Building the route for modify entry API
    3.writing the codes to test the modify entry
    
    with this feature the following has been done to test the Modify Entry API functionalities:
    4.run the test of the codes of modify entry
    5.also testing the Modify entry API functionalities in POSTMAN
    
     [Delivers #169378224]

--- server/controllers/entryControllers.js
+++ server/controllers/entryControllers.js
@@ -7,6 +7,7 @@ const entries = [];
 
 
 class Controller4entry {
+  // Add Entry
   static createEntry = (req, res) => {
     let {
       title, description,
@@ -20,6 +21,44 @@ class Controller4entry {
       status: 200,
       message: 'Entry created successfully',
       data: entry,
+    });
+  }
+
+  // Modify Entry 
+  static editEntry = (req, res) => {
+    let {
+      title, description,
+    } = req.body;
+    let { entryId } = req.params;
+    if (isNaN(entryId)) {
+      return res.status(400).send({
+        status: 400,
+        error: 'The entry id should be a number',
+      });
+    }
+    const authEmail = emailDecrypt(req.header('authorization'));
+
+    const editableEntry = entries.find((entry) => entry.id === parseInt(entryId, 10));
+    if (!editableEntry) {
+      return res.status(404).send({
+        status: 404,
+        message: 'Non existing text entry',
+      });
+    }
+    if (editableEntry.userEmail !== authEmail) {
+      return res.status(403).send({
+        status: 403,
+        message: 'You are not authorized to take this action!',
+      });
+    }
+
+    editableEntry.title = title;
+    editableEntry.description = description;
+
+    return res.status(200).send({
+      status: 200,
+      message: 'Story was edited successfully',
+      data: editableEntry,
     });
   }
 }
